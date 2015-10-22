abfafea0f1495f5c23ec424216767c519951e1f2
jandrei <jandrei.cm@gmail.com>
2015-10-22 20:42:01 -0200

    exemplo rn

--- src/com/senac/biblioteca/rn/EmprestimoRN.java
+++ src/com/senac/biblioteca/rn/EmprestimoRN.java
@@ -5,10 +5,47 @@
  */
 package com.senac.biblioteca.rn;
 
+import com.senac.biblioteca.bd.ConexaoBD;
+import com.senac.biblioteca.bd.EmprestimoBD;
+import com.senac.biblioteca.bean.Emprestimo;
+import com.senac.biblioteca.bean.Livro;
+import com.senac.biblioteca.bean.Usuario;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author ruans
  */
-public class EmprestimoRN  {
-    
+public class EmprestimoRN {
+
+    EmprestimoBD bd;
+
+    public EmprestimoRN() {
+        bd = new EmprestimoBD();
+
+    }
+
+    public void salvar(Emprestimo emprestimo) {
+        bd.salvar(emprestimo);
+    }
+
+    public void devolucao(Emprestimo emprestimo) {
+        bd.devolucao(emprestimo);
+    }
+
+    public List<Emprestimo> listaEmprestimoDoUsuario(Usuario usuario) {
+        return bd.listaEmprestimoDoUsuario(usuario);
+    }
+
+    public void excluir(Usuario p_usuario) {
+        bd.excluir(p_usuario);
+    }
 }
